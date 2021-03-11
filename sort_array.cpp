754f497e27c28cfb815688a8e0fbfb26b7661b2d
Somnath Chakraborty <61010000+somnath352@users.noreply.github.com>
2021-03-12 02:10:06 +0530

    sort the array

---
+++ sort_array.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int a[n];
+		for(int i=0;i<n;i++)
+			cin>>a[i];
+		int c0=0,c1=0,c2=0;
+		for(int i=0;i<n;i++)
+		{
+			if(a[i]==0)
+				c0++;
+			else if(a[i]==1)
+				c1++;
+			else
+				c2++;
+		}
+		int k=0;
+		for(int i=0;i<c0;i++) a[k++]=0;
+		for(int i=0;i<c1;i++) a[k++]=1;
+		for(int i=0;i<c2;i++) a[k++]=2;
+
+		for(int i=0;i<n;i++)
+			cout<<a[i]<<" ";
+			cout<<"\n";
+	}
+	return 0;
+}
